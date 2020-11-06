e1090f2937d8115412f56afd347a41b7aaed06d9
Mert Derman <mert@merts-mac-mini.home>
2020-11-06 15:50:45 +0000

    Basket class created. It can scan and return total price

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -1,13 +1,36 @@
-using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Checkout.Tests
 {
     public class BasketTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Dictionary<string, decimal> _prices;
+
+        public BasketTests()
         {
+            _prices = new Dictionary<string, decimal>
+            {
+                {"A99", 0.5M},
+                {"B15", 0.3M},
+                {"C40", 0.6M}
+            };
+        }
+        
+        [Fact]
+        public void ReturnsZeroIfNoItemsScanned()
+        {
+            var basket = new Basket(_prices);
+            Assert.Equal(0.0M,basket.GetTotal());
+        }
+        
+        [Fact]
+        public void ReturnsTotalForTwoItems()
+        {
+            var basket = new Basket(_prices);
+            basket.Scan("B15");
+            basket.Scan("A99");
+            Assert.Equal(0.8M,basket.GetTotal());
         }
     }
 }
--- Checkout/Basket.cs
+++ Checkout/Basket.cs
@@ -1,8 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkout
 {
     public class Basket
     {
+        private readonly Dictionary<string, decimal> _prices;
+        private readonly Dictionary<string, int> _items;
+
+        public Basket(Dictionary<string, decimal> prices)
+        {
+            _prices = prices;
+            _items = new Dictionary<string, int>();
+        }
+        
+        public void Scan(string sku)
+        {
+            if (_items.ContainsKey(sku))
+            {
+                _items[sku] += 1;
+                return;
+            }
+            _items.Add(sku,1);
+        }
+        
+        public decimal GetTotal()
+        {
+            decimal total = 0;
+            total += _items.Sum(item => _prices[item.Key] * item.Value);
+            return total;
+        }
     }
 }
